7fce9c6f790891099f8e8f54287ffb2e204bf4ba
saya <iskenderova.bermet19@gmail.com>
2024-01-24 21:00:23 +0600

    Second commit

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
-public class Main {
-    public static void Main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        Scanner scanner= new Scanner(System.in);
+        int wholeWay = 7;
+        int[] myArray = new int [wholeWay];
+        int at1 = scanner.nextInt()-1;
+        int at2= scanner.nextInt()-1;
+        int at3= scanner.nextInt()-1;
+
+        int firstBox = 237;
+        int secondBox = 300;
+        int thirdBox = 176;
+        
 
     }
 }
