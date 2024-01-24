6348aaf5df18931707fd1f904dc0546704f4909e
saya <iskenderova.bermet19@gmail.com>
2024-01-24 23:16:20 +0600

    Third  commit

--- Main.java
+++ Main.java
@@ -7,28 +7,31 @@ public class Main
     {
         Random random = new Random();
         Scanner scanner= new Scanner(System.in);
-        int wholeWay = 7;
-        int[] myArray = new int [wholeWay];
-        int at1 = scanner.nextInt()-1;
-        int at2= scanner.nextInt()-1;
-        int at3= scanner.nextInt()-1;
-
+        int[] myArray = new int [7];
+        int[] km_inputs = new int[3];
         int count= 0;
-        for (int i = 0; i<myArray.length; i++ ){
-            if(count<3) {
-                myArray[i] = random(myArray[i]);
-                if(myArray[i]==1){
-                    count++;
-                }
-            }
-            if(i>=5 && count!=3 ){
-                myArray[i]=1;
-            }
-            System.out.print(myArray[i]+" ");
-            if(count>3){
-                myArray[i]=0;
-            }
+
+        for (int i = 0; i<myArray.length; i++ ) {
+            again_hahaha(myArray[i],count);
         }
+        int trues=0;
+         while (trues ==3){
+             for (int i= 5; i>0 ; i--){
+                 for (int km=3; km>0; km--) {
+                     int user = scanner.nextInt() - 1;
+                     if (myArray[user] == 1) {
+                         trues++;
+                     }
+                 }
+                 if(trues==3){
+                     System.out.println("Correctly entered spots: "+ trues);
+                     
+                 }
+             }
+         }
+
+
+
 
     }
     public static  int random(int a){
@@ -37,4 +40,27 @@ public class Main
         return a;
 
     }
+    public static int checking_the_cargo( int km_array, int howmanyright ){
+        if(km_array==1){
+            howmanyright++;
+        }
+        return howmanyright;
+    }
+     public static int again_hahaha(int a, int b ){
+        Random random = new Random();
+        a= random.nextInt(2);
+        if(a==1){
+            if(b<3){
+                a=1;
+                b++;
+            }
+            else if (b!=3 && b>3){
+                a=0;
+            }
+        }
+        return a;
+
+     }
+
+
 }
