c540307f8fc6290f0d442f6f8049cf3059501782
saya <iskenderova.bermet19@gmail.com>
2024-01-24 21:32:28 +0600

    random works well)

--- Main.java
+++ Main.java
@@ -13,10 +13,28 @@ public class Main
         int at2= scanner.nextInt()-1;
         int at3= scanner.nextInt()-1;
 
-        int firstBox = 237;
-        int secondBox = 300;
-        int thirdBox = 176;
-        
+        int count= 0;
+        for (int i = 0; i<myArray.length; i++ ){
+            if(count<3) {
+                myArray[i] = random(myArray[i]);
+                if(myArray[i]==1){
+                    count++;
+                }
+            }
+            if(i>=5 && count!=3 ){
+                myArray[i]=1;
+            }
+            System.out.print(myArray[i]+" ");
+            if(count>3){
+                myArray[i]=0;
+            }
+        }
+
+    }
+    public static  int random(int a){
+        Random random = new Random();
+        a = random.nextInt(2);
+        return a;
 
     }
 }
