ef1da2767edf18df8eb0ae367b2290a5b689dd21
saya <iskenderova.bermet19@gmail.com>
2024-01-25 00:23:06 +0600

    Works , but not well as needed, Sixth commit

--- Main.java
+++ Main.java
@@ -1,28 +1,32 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner= new Scanner(System.in);
         int[] myArray = new int [7];
-        int[] km_inputs = new int[3];
-        int count= 0;
+        int[] km_inputs= new int[3];
 
-        for (int i = 0; i<myArray.length; i++ ) {
-            give_values(myArray[i], count);
-            System.out.print(myArray[i]+" ");
+        for (int i =0; i<km_inputs.length; i++){
+            km_inputs[i]=scanner.nextInt()-1;
         }
-        System.out.println(count);
+        for (int i = 0; i<myArray.length; i++ ) {
+            int count= 0;
+            myArray[i]= random.nextInt(2);
+            if(myArray[i]==1 && count<3){
+                count ++;
+            }
+            else if(myArray[i]==1 && count>3){
+                myArray[i]=0;
+            }
+        }
         int trues=0;
          while (trues ==3){
              for (int i= 5; i>0 ; i--){
-                 for (int km=3; km>0; km--) {
-                     int user = scanner.nextInt() - 1;
-                     if (myArray[user] == 1) {
+                 for (int j=0; j< km_inputs.length  ; j++) {
+                     int x = km_inputs[j];
+                     if(x == myArray[x] && myArray[x]==1){
                          trues++;
                      }
                  }
@@ -31,42 +35,21 @@ public class Main
                      System.out.println("Congratulations, You found all boxes!");
                      break;
                  }
+                 else {
+                     System.out.println("Correctly entered spots: "+ trues +" try ine more time!");
+                 }
              }
              System.out.println("Boxes changed their locations (((");
              for (int i = 0; i<myArray.length; i++ ) {
-                 give_values(myArray[i],count);
+                 int count= 0;
+                 myArray[i]= random.nextInt(2);
+                 if(myArray[i]==1 && count<3){
+                     count ++;
+                 }
+                 else if(myArray[i]==1 && count>3){
+                     myArray[i]=0;
+                 }
              }
-
          }
     }
-    public static  int random(int a){
-        Random random = new Random();
-        a = random.nextInt(2);
-        return a;
-
-    }
-    public static int checking_the_cargo( int km_array, int howmanyright ){
-        if(km_array==1){
-            howmanyright++;
-        }
-        return howmanyright;
-    }
-     public static int give_values(int a, int x){
-        Random random = new Random();
-        int[] local = new int [7];
-        x=0;
-        for (int i = 0; i< local.length;i++) {
-            local[i] = random.nextInt(2);
-            a = local[i];
-            if (a == 1) {
-                if (x < 3 && a==1) {
-                    a = 1;
-                    x++;
-                } else if (x== 3 && a==1) {
-                    a = 0;
-                }
-            }
-            return a;
-        }
-     }
 }
